5bbde74d34c686d00593c224a614f2dd2d9598a8
AlanaZulke <a.zulke@lancaster.ac.uk>
2018-10-30 11:58:38 +0000

    program test

---
+++ functions.py
@@ -0,0 +1,11 @@
+from typing import Any, Union
+
+
+def square(x):
+    result = x * x
+    return result
+
+
+def coulomb(x):
+    result = 1/x
+    return result
---
+++ test_mysoftware.py
@@ -0,0 +1,13 @@
+from mysoftware import *
+
+
+def test_square_integers():
+    assert square(2) == 4
+    assert square(0) == 0
+    assert square(-1) == 1
+    assert square(3) == 9
+
+def test_coulomb():
+	assert coulomb(1.0) == 1.0
+	assert couloumb(2.0) == 2.0
+
